b4eecb9da6eb3796a8b3ed9bebf872eef8a55c0a
suvantha27-tech <suvantha27@gmail.com>
2026-09-22 20:44:26 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,3 @@
 self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open("tk-cache").then(cache=>{
-      return cache.addAll(["/"]);
-    })
-  );
-});
-
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=>{
-      return res || fetch(e.request);
-    })
-  );
+caches.open("tk").then(cache=>cache.addAll(["/"]));
 });
